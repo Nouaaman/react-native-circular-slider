e8bb2c7fb82856884fc89fb4c1b1e9075c48e938
Nouaaman <nouaaman.dev@gmail.com>
2025-03-13 22:04:48 +0100

    initialize Expo project with TypeScript configuration and basic layout

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function _layout() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
+
+        </View>
+    )
+}
+
+export default _layout
