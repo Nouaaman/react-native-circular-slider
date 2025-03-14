f8fc561d0bed39cb9b2873521bcbfbb0f40749d2
Nouaaman <nouaaman.dev@gmail.com>
2025-03-14 04:55:20 +0100

    update images and enhance CircularSlider with animation effects and layout improvements

--- components/CircularSlider.tsx
+++ components/CircularSlider.tsx
@@ -1,7 +1,7 @@
 import { View, Dimensions, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import SliderItem from './SliderItem'
-import Animated, { runOnJS, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
+import Animated, { clamp, FadeIn, FadeOut, runOnJS, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
 
 const images = [
     require('../assets/images/1.jpg'),
@@ -20,15 +20,20 @@ export default function CircularSlider() {
     const [activeIndex, setActiveIndex] = React.useState(0)
     const scrollX = useSharedValue(0)
     const onSroll = useAnimatedScrollHandler((e) => {
-        scrollX.value = e.contentOffset.x / (_itemSize + _spacing)
+        scrollX.value = clamp(e.contentOffset.x / (_itemSize + _spacing), 0, images.length - 1)
         const newActiveIndex = Math.round(scrollX.value)
         if (activeIndex !== newActiveIndex) runOnJS(setActiveIndex)(newActiveIndex)
     })
 
     return (
         <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-            <View style={[StyleSheet.absoluteFill, { justifyContent: 'center', alignItems: 'center' }]}>
-                <Image source={images[activeIndex]} style={{ width: '100%', height: '100%' }} />
+            <View style={[StyleSheet.absoluteFill, { justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }]}>
+                <Animated.Image
+                    entering={FadeIn.duration(500)}
+                    exiting={FadeOut.duration(500)}
+                    key={`img-${activeIndex}`}
+                    source={images[activeIndex]}
+                    style={{ width: '100%', height: '100%' }} />
             </View>
 
             <Animated.FlatList
